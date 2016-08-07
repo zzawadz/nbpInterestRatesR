22a507926135fe63e31c4ada9b0a55421264828a
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-08-07 10:42:04 +0200

    bump coverage

--- tests/testthat/test-basics.R
+++ tests/testthat/test-basics.R
@@ -17,6 +17,15 @@ test_that("Get max loan interest rate",
 {
   x = get_nbp_interest_rates()
   expect_error(expand_daily(x, "1998-01-01"))
+
+  expect_s3_class(expand_daily(x), "xts")
 })
 
+test_that("Expand to dataframe",
+{
+  x = get_nbp_interest_rates()
+
+  xx = xts2tbl(x)
+  expect_equal(class(xx[["date"]]),"Date")
+})
 
